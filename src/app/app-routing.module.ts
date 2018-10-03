518eb192946412e73aa7dd5b81b4df7e3e1207ed
Priya Singh <pscs1160@gmail.com>
2018-10-04 01:44:21 +0530

    Changes for user details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import {UsersComponent} from './users/users.component';
+import {DetailsComponent} from './details/details.component';
+import {PostsComponent} from './posts/posts.component';
+const routes: Routes = [
+  {
+    path : '',
+    component : UsersComponent
+  },
+  {
+    path : 'details/:id',
+    component : DetailsComponent
+  },
+  {
+    path : 'posts',
+    component : PostsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
